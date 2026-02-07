9f3189d2e4d0c572fb4695540a45e724318663fb
unicellye-eng <unicell.ye@gmail.com>
2026-02-08 00:18:35 +0300

    Delete register-sw.js

--- register-sw.js
+++
@@ -1,9 +0,0 @@
-// Registers the service worker that forces fresh fetches for key assets.
-(() => {
-  if (!('serviceWorker' in navigator)) return;
-
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js', { scope: './' })
-      .catch((err) => console.warn('[SW] registration failed:', err));
-  });
-})();
